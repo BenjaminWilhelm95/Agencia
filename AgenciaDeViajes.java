667710fa896c128cb8562f1cf603fe24e0472e4f
BenjaminWilhelm95 <132071911+BenjaminWilhelm95@users.noreply.github.com>
2023-06-22 14:08:45 -0400

    Add files via upload

---
+++ AgenciaDeViajes.java
@@ -0,0 +1,25 @@
+public class AgenciaDeViajes {
+    private String nombre;
+    private String direccion;
+
+    public AgenciaDeViajes(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
